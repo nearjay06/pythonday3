b4132718d47c9e0ac5e8fa4c9b895e96245310db
nearjay06 <nearjoan2018@gmail.com>
2018-09-05 16:52:31 +0300

    edit sortlist

---
+++ missingnumbers.py
@@ -0,0 +1,24 @@
+def numbers():
+
+ bunchlist =[1,3,4,6,8,12,15,18,20]
+ xlist = []
+
+ for i in range (1,21):
+  if i not in bunchlist:
+   xlist.append (i)
+
+   return xlist  
+     
+  else:
+    print ('invalid')
+
+for element in range(numbers):
+        if number % element == 0:
+         
+
+    return True
+
+
+
+     
+numbers()
