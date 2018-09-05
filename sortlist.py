b18c5f7e0c8be850649182f2d269fc32795debfa
nearjay06 <nearjoan2018@gmail.com>
2018-09-05 17:03:36 +0300

    edit sortlist

--- sortlist.py
+++ sortlist.py
@@ -1,20 +1,22 @@
 def stuff ():
- lola_list = [1,2,3,4,5,6,7,8,'a','b','c','d','e']
+ lola_list = [9,10,1,2,3,4,5,6,7,8,'a','b','c','d','e']
  lolalist = []
  lolalist1 =[]
  lolalist2=[]
  mydict= dict()
  for i in lola_list:
   if isinstance (i,str):
-   lolalist.append(i)
-   mydict['characters']=lolalist
-  elif isinstance (i,int):
+   lolalist.append(i)     
+   mydict['characters']= sorted(lolalist)
+  elif isinstance (i,int):      
    if i% 2 == 0:
-    lolalist1.append(i)
-    mydict['evens'] = lolalist1
+    lolalist1.append(i)    
+    mydict['evens'] = sorted(lolalist1)
    elif i%2==1:
     lolalist2.append(i)
-    mydict['odds']=lolalist2
+    mydict['odds']= sorted(lolalist2)
+
+    
 
    else:
     print ("invalid")
